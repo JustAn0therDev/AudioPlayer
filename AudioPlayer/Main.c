d4f18398e861ddb26cd5e22dfbe818a11cad8033
Ruan Montelo <ruanmontelo@gmail.com>
2023-03-21 19:25:35 -0300

    Removed the test code for opening wave MCI

--- AudioPlayer/Main.c
+++ AudioPlayer/Main.c
@@ -11,11 +11,10 @@ const int BTN_HEIGHT = 100;
 
 HWND hwndPlayButton = { 0 };
 HWND hwndMain = { 0 };
-HWND hwndStopButton = { 0 };
 
 BOOL playing = FALSE;
 
-LPHWAVEOUT phwo = 0;
+LPCWSTR lpszWAVEFileName = L"D:\\repos\\AudioPlayer\\AudioPlayer\\TestAudio\\PinkPanther30.wav";
 
 LRESULT BtnPlayWndProc(
 	HWND hInstance,
@@ -41,8 +40,6 @@ LRESULT WndProc(
 		case WM_COMMAND: {
 			if (lParam == (LPARAM)hwndPlayButton) {
 				return BtnPlayWndProc(hInstance, WM_LBUTTONDOWN, wParam, lParam);
-			} else if (lParam == (LPARAM)hwndStopButton) {
-				return BtnStopWndProc(hInstance, WM_LBUTTONDOWN, wParam, lParam);
 			}
 		}
 
@@ -82,120 +79,10 @@ LRESULT BtnPlayWndProc(
 ) {
 	switch (msg) {
 	case WM_LBUTTONDOWN: {
-		PlaySound(
-			L"D:\\repos\\AudioPlayer\\AudioPlayer\\TestAudio\\PinkPanther30.wav",
-			0,
-			SND_FILENAME | SND_ASYNC);
+		if (playing == TRUE) {
+			PlaySound(0, 0, 0);
 
-		// Information to pass in to the "waveOutOpen" function.
-		WAVEFORMATEX pwfx;
-		pwfx.cbSize = sizeof(WAVEFORMATEX);
-		pwfx.wFormatTag = WAVE_FORMAT_PCM;
-		pwfx.nChannels = 2;
-		pwfx.nSamplesPerSec = 44100;
-		pwfx.wBitsPerSample = 16;
-		pwfx.nBlockAlign = pwfx.nChannels * pwfx.wBitsPerSample;
-		pwfx.nAvgBytesPerSec = pwfx.nSamplesPerSec * pwfx.nBlockAlign;
-
-		//MMRESULT waveOutOpen(
-		//	LPHWAVEOUT      phwo,
-		//	UINT            uDeviceID,
-		//	LPCWAVEFORMATEX pwfx,
-		//	DWORD_PTR       dwCallback,
-		//	DWORD_PTR       dwInstance,
-		//	DWORD           fdwOpen
-		//);
-
-		MMRESULT mmr = waveOutOpen(
-			phwo, 
-			WAVE_MAPPER,
-			&pwfx,
-			CALLBACK_NULL,
-			0,
-			WAVE_MAPPED_DEFAULT_COMMUNICATION_DEVICE
-			);
-
-		switch (mmr) {
-		case MMSYSERR_ALLOCATED:
-		{
-			OutputDebugStringA("Specified resource is already allocated.\n");
-		}
-		break;
-		case MMSYSERR_BADDEVICEID:
-		{
-			OutputDebugStringA("Specified device identifier is out of range.\n");
-		}
-		break;
-		case MMSYSERR_NODRIVER:
-		{
-			OutputDebugStringA("No device driver is present.\n");
-		}
-		break;
-		case MMSYSERR_NOMEM:
-		{
-			OutputDebugStringA("Unable to allocate or lock memory.\n");
-		}
-		break;
-		case WAVERR_BADFORMAT:
-		{
-			OutputDebugStringA("Attempted to open with an unsupported waveform-audio format.\n");
-		}
-		break;
-		case WAVERR_SYNC:
-		{
-			OutputDebugStringA(
-				"The device is synchronous but waveOutOpen was called without using the"\
-				"WAVE_ALLOWSYNC flag.\n"
-			);
-		}
-		break;
-		case MMSYSERR_NOERROR:
-		{
-			OutputDebugStringA("Everything is A OKAY\n");
-			
-		}
-		case MMSYSERR_ERROR:
-		{
-			OutputDebugStringA("Something bad happened\n");
-		}
-		case MMSYSERR_INVALFLAG:
-		{
-			OutputDebugStringA("Invalid flag!\n");
-		}
-		break;
-		default:
-			OutputDebugStringA("Nobody is sure what happened.\n");
-			break;
-		}
-
-		if (playing == FALSE) {
 			DestroyWindow(hwndPlayButton);
-
-			hwndPlayButton = 0;
-
-			hwndPlayButton = CreateWindow(
-				L"BUTTON",  // Predefined class; Unicode assumed 
-				L"PAUSE",    // Button text 
-				WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // Styles 
-				(WINDOW_WIDTH / 2) - (BTN_WIDTH / 2),        // x position 
-				(WINDOW_HEIGHT / 2) - (BTN_HEIGHT),        // y position 
-				BTN_WIDTH,				      // button size (width)
-				BTN_HEIGHT,				      // button size (height)
-				hwndMain,				  // Parent window
-				0,                        // No menu.
-				(HINSTANCE)GetWindowLongPtr(hwndMain, GWLP_HINSTANCE),
-				0);                       // Pointer not needed.
-
-			if (hwndPlayButton == 0) {
-				return 1;
-			}
-
-			playing = TRUE;
-		}
-		else {
-			DestroyWindow(hwndPlayButton);
-
-			hwndPlayButton = 0;
 
 			hwndPlayButton = CreateWindow(
 				L"BUTTON",  // Predefined class; Unicode assumed 
@@ -216,10 +103,37 @@ LRESULT BtnPlayWndProc(
 
 			playing = FALSE;
 		}
+		else {
+			PlaySound(
+				lpszWAVEFileName,
+				0,
+				SND_FILENAME | SND_ASYNC);
+
+			DestroyWindow(hwndPlayButton);
+
+			hwndPlayButton = CreateWindow(
+				L"BUTTON",  // Predefined class; Unicode assumed 
+				L"STOP",    // Button text 
+				WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // Styles 
+				(WINDOW_WIDTH / 2) - (BTN_WIDTH / 2),        // x position 
+				(WINDOW_HEIGHT / 2) - (BTN_HEIGHT),        // y position 
+				BTN_WIDTH,				      // button size (width)
+				BTN_HEIGHT,				      // button size (height)
+				hwndMain,				  // Parent window
+				0,                        // No menu.
+				(HINSTANCE)GetWindowLongPtr(hwndMain, GWLP_HINSTANCE),
+				0);                       // Pointer not needed.
+
+			if (hwndPlayButton == 0) {
+				PostQuitMessage(0);
+				return 1;
+			}
+
+			playing = TRUE;
+		}
 
 		return 0;
 	}
-
 	default:
 	{
 		return DefWindowProc(hInstance, msg, wParam, lParam);
@@ -300,30 +214,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		return 1;
 	}
 
-	hwndStopButton = CreateWindow(
-		L"BUTTON",  // Predefined class; Unicode assumed 
-		L"STOP",    // Button text 
-		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // Styles 
-		(WINDOW_WIDTH / 2) - (BTN_WIDTH / 2),        // x position 
-		(WINDOW_HEIGHT / 2) + 155,        // y position 
-		BTN_WIDTH,				      // button size (width)
-		BTN_HEIGHT,				      // button size (height)
-		hwndMain,				  // Parent window
-		0,                        // No menu.
-		(HINSTANCE)GetWindowLongPtr(hwndMain, GWLP_HINSTANCE),
-		0);                       // Pointer not needed.
-
-	if (hwndStopButton == 0) {
-		PostQuitMessage(0);
-		return 1;
-	}
-
 	ShowWindow(hwndMain, nCmdShow);
 
 	MSG msg;
-
-	// This is called before handling any sound request
-	phwo = malloc(sizeof(LPHWAVEOUT));
 
 	GetMessage(&msg, 0, 0, 0);
 
@@ -333,7 +226,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	}
 
 	DestroyWindow(hwndMain);
-	free(phwo);
 
 	return (int)msg.wParam;
 }
